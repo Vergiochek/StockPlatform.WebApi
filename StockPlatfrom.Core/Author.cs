72f9a3bb881e27b5d5e25c4463b31bd98e646920
Vergiochek <62502874+Vergiochek@users.noreply.github.com>
2021-10-27 23:07:22 +0300

    Author class created

---
+++ StockPlatfrom.Core/Author.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace StockPlatfrom.Core
+{
+    public class Author
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public int Age { get; set; }
+        public string DateOfAccountCreation { get; set; }
+    }
+}
